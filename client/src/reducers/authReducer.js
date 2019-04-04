95dba5a41efabddd30d12c05d4fdb72dcfb63f08
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2019-04-04 17:44:02 -0400

    adding json database

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,15 +2,16 @@ import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 
 const INTIAL_STATE = {
-	isSingedIn: null
+	isSignedIn: null,
+	userId: null
 };
 
 export default (state = INTIAL_STATE, action) => {
 	switch (action.type) {
 		case SIGN_IN: 
-			return { ...state, isSingedIn: true };
+			return { ...state, isSignedIn: true, userId: action.payload };
 		case SIGN_OUT: 
-			return { ...state, isSingedIn: false}
+			return { ...state, isSignedIn: false, userId: null };
 		default: 
 			return state;
 	}
